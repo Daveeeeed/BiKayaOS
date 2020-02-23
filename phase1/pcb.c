bbdf3e961453aa0da7c5093c95e670b945641be0
Daveeeeed <dati981026@gmail.com>
2020-02-23 17:45:57 +0100

    Last review and comment required

--- phase1/pcb.c
+++ phase1/pcb.c
@@ -8,19 +8,16 @@ HIDDEN LIST_HEAD(pcb_free);
 
 HIDDEN pcb_t pcb_table[MAXPROC];
 
-/* OKAY */
 void initPcbs(void){
    for (int i = 0; i < MAXPROC; i++){
        list_add(&pcb_table[i].p_next, &pcb_free);
    }
 }
 
-/* OKAY */
 void freePcb(pcb_t *p){
    list_add_tail(&p->p_next, &pcb_free); 
 }
 
-/* OKAY */
 pcb_t *allocPcb(void){
     if(list_empty(&pcb_free)){
         return NULL;
@@ -35,30 +32,40 @@ pcb_t *allocPcb(void){
     }
 }
 
-/* OKAY */
 void mkEmptyProcQ(struct list_head *head){
     INIT_LIST_HEAD(head);
 }
 
-/* OKAY */
 int emptyProcQ(struct list_head *head){
     return (list_empty(head));
 }
 
-/* DA CONTROLLARE */
-void insertProcQ(struct list_head *head, pcb_t *p){
-    struct pcb_t *test1;
-    test1 = container_of(head->next, struct pcb_t, p_next);
-    if(p->priority > test1->priority ||  p->priority == test1->priority || test1->priority == NULL ){
-        list_add(&(p->p_next), head);
-    } else {
-       struct list_head *transm;
-       transm = (head->next);
-       insertProcQ(transm,p);
+void insertProcQ(struct list_head *q, struct pcb_t *p){ 
+    if ( q == NULL || p == NULL)
+        return;
+
+    struct list_head *aux, *old;
+    struct pcb_t *tmp;
+
+    old = q;
+    aux = q->next;
+
+    while (aux != q && aux != NULL) {
+        tmp = container_of(aux, struct pcb_t, p_next);
+        if (tmp->priority < p->priority)
+            break;
+
+        old = aux;
+        aux = aux->next;
     }
+        
+    if (aux == q)
+        list_add_tail( &(p->p_next) , q );
+    else
+        list_add( &(p->p_next) , old );
+
 }
 
-/* DA CONTROLLARE */
 pcb_t *headProcQ(struct list_head *head){
     struct pcb_t *test1;
     test1 = container_of(head->next, struct pcb_t, p_next);
@@ -73,7 +80,6 @@ pcb_t *headProcQ(struct list_head *head){
     else return test1;
 }
 
-/* DA CONTROLLARE */
 pcb_t *removeProcQ(struct list_head *head){
     pcb_t *ret; 
     ret = headProcQ(head);
@@ -81,7 +87,6 @@ pcb_t *removeProcQ(struct list_head *head){
     return ret;
 }
 
-/* DA CONTROLLARE */
 pcb_t *outProcQ(struct list_head *head, pcb_t *p){
     if(emptyProcQ(head)){
         return FALSE;
@@ -107,25 +112,25 @@ pcb_t *outProcQ(struct list_head *head, pcb_t *p){
     }
 }
 
-//true se p non ha figli, false se ne ha almeno uno
-int emptyChild(struct pcb_t *p) {
-    if( p->p_child.next == NULL ) return 1; // un list_head i cui campi puntano a NULL si intende come una lista vuota
-    else return list_empty( &(p->p_child) );
+int emptyChild(pcb_t *this){
+    if (this != NULL){
+        if (this->p_child.next == NULL){
+            return TRUE;
+        } else {
+            return list_empty(&this->p_child);
+        }
+    }
+    return ERROR;
 }
  
-
-//Rende p un figlio di parent, inserendolo nella sua lista di processi figli
-void insertChild(struct pcb_t *parent, struct pcb_t *p) {
-    if (parent == NULL || p == NULL) return;
-    if( parent->p_child.next == NULL ) {	//Se i puntatori sono ancora a NULL li inizializziamo correttamente
-        INIT_LIST_HEAD( &parent->p_child );
+void insertChild(pcb_t *prnt, pcb_t *p){
+    if (prnt != NULL && p != NULL){
+        if (emptyChild(prnt)) INIT_LIST_HEAD(&prnt->p_child);
+        list_add_tail(&p->p_sib, &prnt->p_child);
     }
-    list_add_tail( &(p->p_sib), &(parent->p_child) ); 
-    p->p_parent = parent;
+    p->p_parent = prnt;
 }
 
-
-//Rimuove p dalla lista dei figli di suo padre
 struct pcb_t *outChild(struct pcb_t *p) {
     if ( p == NULL || p->p_parent == NULL ) return NULL; 
     list_del( &(p->p_sib) );   
@@ -133,16 +138,11 @@ struct pcb_t *outChild(struct pcb_t *p) {
     return p;
 }
 
-
- 
-//Rimuove il primo processo figlio di p
-struct pcb_t *removeChild(struct pcb_t *p) {
-    if ( p == NULL || emptyChild(p) ) return NULL;
-    struct pcb_t *removedChild;
-    struct list_head *q = &p->p_child;
-    if (q == NULL || list_empty(q)) return NULL; //return NULL se q è NULL o la lista dei figli di p è vuota
-    removedChild = container_of( q->next, typeof(*removedChild), p_sib );
-    list_del(q->next);
-    removedChild->p_parent = NULL;
-    return removedChild;
+pcb_t* removeChild(pcb_t *p){ 
+    if(p == NULL) return NULL;
+    if(emptyChild(p)) return NULL;
+    pcb_t *target = container_of(p->p_child.next, pcb_t, p_sib);
+    list_del(&target->p_sib);
+    target->p_parent = NULL;
+    return target;
 }
